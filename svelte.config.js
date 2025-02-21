02d03fc1f7ef9b6e139ee329abdfba6344fb0c80
Kristian <kristian@wbprosjekt.no>
2025-02-21 09:32:39 +0100

    oppdatering med env

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $lib: 'src/lib'
+    }
+  },
+  preprocess: preprocess()
 };
-
-export default config;
