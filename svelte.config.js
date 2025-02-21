d26a96bec5cdaa4fe9f2bbac8a839bf5d9dae178
Kristian <kristian@wbprosjekt.no>
2025-02-21 09:54:09 +0100

    Fixed Vercel adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,7 @@ import preprocess from 'svelte-preprocess';
 
 export default {
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x'  // Correct runtime format for Vercel
-    })
+    adapter: adapter()
   },
   preprocess: preprocess()
 };
